a1cc65138c5dc1026f3149721ea0791bf8b2cb76
Uten Lapanikorn <vasana2547a@gmail.com>
2024-07-24 14:40:56 +0700

    งาน function

---
+++ ep2/index.ts
@@ -0,0 +1,15 @@
+function grade(homework: number, midterm: number, final: number){
+    let score: number = homework+midterm+final
+    if(score<50){
+        return 'เกรด F'
+}else if(score<60){
+        return 'เกรด D'
+}else if(score<70){
+        return 'เกรด C'
+}else if(score<80){
+        return 'เกรด B'
+}else if(score>=80){
+        return 'เกรด A'
+    }   
+}
+console.log(grade(45,20,15))
