0173afb5842b0ce6b30d492b1ae2d881e54b3ed8
Uten Lapanikorn <vasana2547a@gmail.com>
2024-08-23 11:22:57 +0700

    เรียนล่าสุด

---
+++ ep5/index.ts
@@ -0,0 +1,83 @@
+// for loop
+
+// let sum: number = 0
+
+// for (let i = 1; i <= 10; i++) {
+//     sum = sum + i
+// }
+  
+//     console.log('total', sum)
+
+// while loop
+
+// let i: number = 1
+// let sum: number = 0
+
+// for (let j = 1; j < 10; j++) {
+//     i = i * 5
+// }
+
+// while (i <= 365) {
+//     sum = sum + i
+//     i++
+// }
+
+// console.log('total', sum)
+
+// let sum: number = 0 
+// let isBeark: boolean = false
+// let row: number = 0
+
+// while (true) {
+//     console.log('โสด')
+//     row++
+//     if (row === 3) {
+//         break
+//     }
+// }
+
+//do while loop
+
+// let i: number = 1
+// let sum: number = 0
+
+// do {
+//     sum = sum + i
+//     i++
+// } while (i <= 365)
+// console.log('total', sum)
+
+// let car1: string = 'Honda'
+// let car2: string = 'Yamaha'
+// let car3: string = 'Toyota'
+// let car4: string = 'Suzaki'
+
+// const cars: string[] = ['Honda', 'Yamaha', 'Toyota', 'Suzaki']
+
+// const num: number[] = [1, 2, 3, 4, 5]
+
+// const bool: boolean[] = [true, false, false, true]
+
+// const myName: string = 'สุดสวย สวยสุด'
+
+// // console.log(cars[2]) // myName.charAt(2)
+
+// // console.log(cars.length)
+
+// cars[1] = 'Mazda'
+
+// console.log(cars[1])
+
+// const cars: string[] = ['Honda', 'Yamaha', 'Toyota', 'Suzuki']
+
+// for (let i = 0; i< cars.length; i++) {
+//     if (cars[i] === 'Yamaha') {
+//     console.log('รถซื้อแกง จะแรงได้ไง')
+//     }
+// }
+const cars: string[] = ['Honda', 'Yamaha', 'Toyota', 'Suzuki']
+
+cars.forEach(function(c) {
+    console.log(c)
+})
+
