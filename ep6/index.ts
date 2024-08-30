7ada3424ef3f70f6779c4b4f37415904c61a4236
Uten Lapanikorn <vasana2547a@gmail.com>
2024-08-30 10:42:14 +0700

    อาทิตย์นี้นะจ้ะ

---
+++ ep6/index.ts
@@ -0,0 +1,40 @@
+// const score: number[] = [10,23,26,30,33]
+
+// let sum: number = 0
+// for (let i = 0; i < score.length; i++) {
+//     sum = sum + score[i]
+// }
+// const avg = sum /score.length
+
+// console.log('Total: ', sum)
+// console.log('Avg: ', avg)
+// // const studentsName: string = 'แนน,ฟู,บี,อ้นจี้,ลัคกี้,นัท,เอ็ม,ออม,ปลื้ม,ก็อต'
+// // const studentsNameArray = studentsName.split(',')
+// // console.log(studentsNameArray.join('9'))
+
+// const animalsName: string = 'หมา*แมว*แพนด้า*เสือ'
+// const animalsNameArray = animalsName.split('*')
+
+// animalsNameArray.pop() // ดึงตัวสุดท้ายออก
+// console.log('ดึงตัวสุดท้ายออก:',animalsNameArray)
+
+// animalsNameArray.push('กาปิบาร่า') // ใส่เข้าไปเพิ่ม
+// console.log('ใส่เข้าไปตัวท้าย:',animalsNameArray)
+
+// animalsNameArray.shift() // ดึงตัวหน้าออก
+// console.log('ดึงตัวหน้าออก:',animalsNameArray)
+
+// animalsNameArray.unshift('แรด')
+// console.log('ใส่ตัวหน้า: ',animalsNameArray)
+
+// console.log('เรียง ก-ฮ:',animalsNameArray.sort())
+
+// console.log('เรียง ฮ-ก',animalsNameArray.reverse())
+
+// console.log('slice 0-2: ',animalsNameArray.slice(0,2))
+
+const fruits = ['Banana','Orange','Apple','Mango']
+
+console.log('เรียง ก-ฮ:',fruits.sort())
+
+console.log('เรียง ฮ-ก',fruits.reverse())
